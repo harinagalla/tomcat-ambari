5f0171c965d71f13bb6bce74ed9fd653b1a0b6ff
harinagalla <harinagalla@users.noreply.github.com>
2016-01-05 11:12:21 -0500

    Create params.py

---
+++ configuration/package/scripts/params.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+from resource_management import *
+from resource_management.libraries.script.script import Script
+import sys, os, glob
+from resource_management.libraries.functions.version import format_hdp_stack_version
+from resource_management.libraries.functions.default import default
+
+# config object that holds the configurations declared in the config xml file
+config = Script.get_config()
+
+tomcat_dirname = 'tomcat-8.0.30'
+
+#params from tomcat-ambari-config
+tomcat_install_dir = config['configurations']['tomcat-ambari-config']['tomcat.install_dir']
+tomcat_port = config['configurations']['tomcat-ambari-config']['tomcat.port']
+tomcat_log = config['configurations']['tomcat-ambari-config']['tomcat.log']
+
+tomcat_dir = os.path.join(*[tomcat_install_dir,tomcat_dirname])
+conf_dir=''
+bin_dir=''
+
+# params from tomcat-bootstrap
+
+tomcat_user = config['configurations']['tomcat-bootstrap-env']['tomcat_user']
+tomcat_group = config['configurations']['tomcat-bootstrap-env']['tomcat_group']
+tomcat_log_dir = config['configurations']['tomcat-bootstrap-env']['tomcat_log_dir']
+tomcat_log_file = os.path.join(tomcat_log_dir,'tomcat_setup.log')
+
+temp_file='/tmp/'+tomcat_dirname+'.zip'
