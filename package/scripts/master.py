089730bee3f53528f328c7f2fd335456ad9076df
harinagalla <harinagalla@users.noreply.github.com>
2016-01-13 13:31:05 -0500

    Update master.py

--- package/scripts/master.py
+++ package/scripts/master.py
@@ -64,7 +64,7 @@ class Master(Script):
     	  Execute('touch ' + params.tomcat_lock_file)
 	  Execute('chown ' + params.tomcat_user + ':' + params.tomcat_group + ' ' + params.tomcat_lock_file)
 	  Execute(params.bin_dir+'/startup.sh >> ' + params.tomcat_log_file, user= params.tomcat_user)
-	  Execute(mkdir -p '+status_params.tomcat_pid_dir)
+	  Execute('mkdir -p '+status_params.tomcat_pid_dir)
 	  Execute('chown -R' + params.tomcat_user + ':' + params.tomcat_group + ' ' +status_params.tomcat_pid_dir)
 	  Execute('ps -ef | grep -i tomcat | awk {\'print $2\'} | head -n 1 > ' + status_params.tomcat_pid_file, user= params.tomcat_user)
 	  
